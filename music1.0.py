59038256ab8f54d390636e0c2fff379dadc1ff8f
kingpin-ay <ayushmondal001@gmail.com>
2022-03-10 17:57:03 +0530

    added the mpv and the python app now able to play a song single time and now you have to close the whole cmd palate to stop the song from playing

--- music1.0.py
+++ music1.0.py
@@ -1,36 +1,47 @@
 # importing nessecery libs
 from base64 import decode
+import subprocess
 import urllib.request as ur
 from bs4 import BeautifulSoup
 import re
+import datetime
+
+# time code
+currentDateTime = datetime.datetime.now()
+date = currentDateTime.date()
+year = date.strftime("%Y")
+month = date.strftime("%M")
+day = date.strftime("%D")
 
 # variables
 countForYoutubeTitles = 0
+
 # Taking the user song input  and converting it to url stream .
 songName = input("Enter the song name : ")
 songNameUrlString = songName.replace(" ", "+")
 
-
 # Youtube Url resource
 searchUrl = rf"https://www.youtube.com/results?search_query={songNameUrlString}"
-
 
 # Taking the response and letting the user choose from the titles of the videos
 
 html = ur.urlopen(searchUrl)
 video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-while countForYoutubeTitles < len(video_ids):
+while countForYoutubeTitles < 3:  # we are going to print first 3 title of the youtube videos we get from the user input search len(video_ids)
     videoUrl = "https://www.youtube.com/watch?v=" + \
         video_ids[countForYoutubeTitles]
     html = ur.urlopen(videoUrl)
     soup = BeautifulSoup(html.read().decode(), 'html.parser')
     rawTitle = str(soup.get_text()).replace(
-        ' - YouTubeAboutPressCopyrightContact usCreatorAdvertiseDevelopersTermsPrivacyPolicy & SafetyHow YouTube worksTest new features© 2022 Google LLC', '')
+        f' - YouTubeAboutPressCopyrightContact usCreatorAdvertiseDevelopersTermsPrivacyPolicy & SafetyHow YouTube worksTest new features© {year} Google LLC',
+        '')
     print(f"{countForYoutubeTitles+1}. {rawTitle}")
     countForYoutubeTitles += 1
 
 choice = int(input("Enter the Video number : "))
-videoUrl = "https://www.youtube.com/watch?v=" + video_ids[choice-1]
+videoUrl = "https://www.youtube.com/watch?v=" + video_ids[choice - 1]
 
-
-
+subprocess.Popen(
+    "start /b " + ".\\mpv-x86_64-20220306-git-1c49d57\\mpv " + videoUrl +
+    " --no-video --loop=inf --input-ipc-server=\\\\.\\pipe\\mpv-pipe > output.txt",
+    shell=True)
